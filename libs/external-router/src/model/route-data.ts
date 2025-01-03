38bf121716ec44e3bbca5876f5c44738b946e081
Damien Retzinger <damienwebdev@gmail.com>
2025-01-03 09:35:43 -0500

    docs(external-router): document known route data (#3378)

--- libs/external-router/src/model/route-data.ts
+++ libs/external-router/src/model/route-data.ts
@@ -2,7 +2,24 @@
  * Data associated with a route.
  */
 export interface DaffRouteData {
+
+  /**
+   * The canonical url of the route.
+   */
   canonical_url?: string;
+
+  /**
+   * The meta description of the route.
+   */
   meta_description?: string;
+
+  /**
+   * The title of the route.
+   */
   title?: string;
+
+  /**
+   * An associated schema for the route.  See {@link Schema} for full capabilities.
+   */
+  schema?: Record<string, unknown>;
 }
